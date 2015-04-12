450fe62705365a0c4dee8819468a648523811ba1
Alessandro dos santos pinto <lastripper@gmail.com>
2015-04-12 18:27:01 -0300

    Initial Commit

---
+++ ADImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADImageView
+//
+//  Created by Alessandro dos santos pinto on 4/12/15.
+//  Copyright (c) 2015 Alessandro dos santos pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
